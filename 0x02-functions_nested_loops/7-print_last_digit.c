fc63deb4556428a47d57b3f07b22726093a4ab22
max-ayo <ogunnnaikeopeyemi2020@gmail.com>
2022-10-19 15:12:06 -0700

    third commit

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -11,7 +11,8 @@ int last_digit;
 last_digit = a %  10;
 if (last_digit < 0)
 {
-last_digit = last_digit * -1;						        	}
+last_digit = last_digit * -1;		 
+}
 _putchar(last_digit + '0');
 return (last_digit);
 
