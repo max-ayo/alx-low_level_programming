2cef47eb97835fdb059389590ed1c8de5973aa8c
max-ayo <ogunnnaikeopeyemi2020@gmail.com>
2022-10-19 15:32:46 -0700

    third commit

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,12 +1,12 @@
-nclude <stdio.h>
+#include <stdio.h>
 /**
-*  * print_to_98 - Prints all natural numbers
-*   * from n to 98.
-*    *
-*     * @n: input number.
-*      *
-*       * Return: no return.
-*        */
+* print_to_98 - Prints all natural numbers
+* from n to 98.
+*
+* @n: input number.
+*
+* Return: no return.
+*/
 void print_to_98(int n)
 {
 if (n > 98)
