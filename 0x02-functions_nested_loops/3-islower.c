f4f0e3467256af652ceb398bc45d96254e58a5c4
max-ayo <ogunnnaikeopeyemi2020@gmail.com>
2022-10-18 17:27:45 -0700

    third commit

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -5,12 +5,13 @@
  * Return:0-if lowercase,1-if not.
  */
 int _islower(int c)
-{		if (c >= 'a' && c <= 'z')
-		{
-							return (1);
-								}
+{
+	if (c >= 'a' && c <= 'z')
+	{
+			return (1);
+	}
 			else
-					{
-							return (0);
-									}
+		{
+			return (0);
+		}
 }
